mshaher9/redux-form-msh

commit 16aac9f469cae8b6f366445829f643fed0428df0

🎉 Flow Typing 🎉 (#3138)
* implemented Flow internally

* added external flow typing for exported interface

* added flowtyped lodash and fixed test coverage

* Removed some unused type definitions

src/createField.js
@@ -1,31 +1,64 @@
+// @flow
 import { Component, createElement } from 'react'
 import PropTypes from 'prop-types'
 import invariant from 'invariant'
 import createConnectedField from './ConnectedField'
 import shallowCompare from './util/shallowCompare'
 import prefixName from './util/prefixName'
+import type {
+  ConnectedComponent,
+  Structure,
+  Event,
+  ReactContext
+} from './types'
+import type { InstanceApi as ConnectedFieldInstanceApi } from './ConnectedField'
 
-const createField = ({ deepEqual, getIn, setIn, toJS }) => {
-  const ConnectedField = createConnectedField({
-    deepEqual,
-    getIn,
-    toJS
-  })
+export type Props = {
+  name: string,
+  component: ReactClass<*> | Function | string,
+  format?: { (value: any, name: string): ?string },
+  normalize?: {
+    (
+      value: any,
+      previousValue: any,
+      allValues: Object,
+      previousAllValues: Object
+    ): ?any
+  },
+  onBlur?: { (event: Event, newValue: any, previousValue: any): void },
+  onChange?: { (event: Event, newValue: any, previousValue: any): void },
+  onDragStart?: { (event: Event): void },
+  onDrop?: { (event: Event, newValue: any, previousValue: any): void },
+  onFocus?: { (event: Event): void },
+  parse?: { (value: any, name: string): any },
+  props?: Object,
+  validate?: { (value: any, allValues: Object, props: Object): ?any },
+  warn?: { (value: any, allValues: Object, props: Object): ?any },
+  withRef?: boolean
+}
+
+const createField = (structure: Structure<*, *>) => {
+  const ConnectedField = createConnectedField(structure)
+
+  const { setIn } = structure
 
   class Field extends Component {
-    constructor(props, context) {
+    props: Props
+    context: ReactContext
+
+    ref: ConnectedComponent<ConnectedFieldInstanceApi>
+
+    constructor(props: Props, context: ReactContext) {
       super(props, context)
       if (!context._reduxForm) {
         throw new Error(
           'Field must be inside a component decorated with reduxForm()'
         )
       }
-
-      this.normalize = this.normalize.bind(this)
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-      return shallowCompare(this, nextProps, nextState)
+    shouldComponentUpdate(nextProps: Props) {
+      return shallowCompare(this, nextProps)
     }
 
     componentWillMount() {
@@ -37,7 +70,7 @@ const createField = ({ deepEqual, getIn, setIn, toJS }) => {
       )
     }
 
-    componentWillReceiveProps(nextProps) {
+    componentWillReceiveProps(nextProps: Props) {
       if (
         this.props.name !== nextProps.name ||
         this.props.validate !== nextProps.validate ||
@@ -59,35 +92,35 @@ const createField = ({ deepEqual, getIn, setIn, toJS }) => {
       this.context._reduxForm.unregister(this.name)
     }
 
-    getRenderedComponent() {
+    saveRef = (ref: ConnectedComponent<ConnectedFieldInstanceApi>) =>
+      (this.ref = ref)
+
+    getRenderedComponent(): React$Component<*, *, *> {
       invariant(
         this.props.withRef,
         'If you want to access getRenderedComponent(), ' +
           'you must specify a withRef prop to Field'
       )
-      return this.refs.connected.getWrappedInstance().getRenderedComponent()
+      return this.ref.getWrappedInstance().getRenderedComponent()
     }
 
-    get name() {
+    get name(): string {
       return prefixName(this.context, this.props.name)
     }
 
-    get dirty() {
+    get dirty(): boolean {
       return !this.pristine
     }
 
-    get pristine() {
-      return this.refs.connected.getWrappedInstance().isPristine()
+    get pristine(): boolean {
+      return this.ref.getWrappedInstance().isPristine()
     }
 
-    get value() {
-      return (
-        this.refs.connected &&
-        this.refs.connected.getWrappedInstance().getValue()
-      )
+    get value(): any {
+      return this.ref && this.ref.getWrappedInstance().getValue()
     }
 
-    normalize(name, value) {
+    normalize = (name: string, value: any): any => {
       const { normalize } = this.props
       if (!normalize) {
         return value
@@ -104,7 +137,7 @@ const createField = ({ deepEqual, getIn, setIn, toJS }) => {
         name: this.name,
         normalize: this.normalize,
         _reduxForm: this.context._reduxForm,
-        ref: 'connected'
+        ref: this.saveRef
       })
     }
   }

src/util/shallowCompare.js
@@ -1,3 +1,4 @@
+// @flow
 import { isEqualWith } from 'lodash'
 
 const customizer = (
@@ -19,11 +20,12 @@ const customizer = (
   }
 }
 
-const shallowCompare = (instance, nextProps, nextState) => {
-  return (
-    !isEqualWith(instance.props, nextProps, customizer) ||
-    !isEqualWith(instance.state, nextState, customizer)
-  )
-}
+const shallowCompare = (
+  instance: { props: any, state?: any },
+  nextProps: Object,
+  nextState?: Object
+): boolean =>
+  !isEqualWith(instance.props, nextProps, customizer) ||
+  !isEqualWith(instance.state, nextState, customizer)
 
 export default shallowCompare

commit 6ba17bc087fff29f7035d13b5ae901fbf5cb9dd1

Fields re-render every time due to regression introduced with flow typings. (#3758)
* Fixes regression introduced in #3138 that no longer checked nextState when determining if a Field should re-render

* Adds test to make sure the render()-function of the field is not called multiple times if neither state nor props have changed

* nextState in shouldComponentUpdate() must be optional

src/createField.js
@@ -33,8 +33,8 @@ const createField = (structure: Structure<*, *>) => {
       }
     }
 
-    shouldComponentUpdate(nextProps: Props) {
-      return shallowCompare(this, nextProps)
+    shouldComponentUpdate(nextProps: Props, nextState?: Object) {
+      return shallowCompare(this, nextProps, nextState)
     }
 
     componentWillMount() {

src/util/shallowCompare.js
@@ -24,8 +24,11 @@ const shallowCompare = (
   instance: { props: any, state?: any },
   nextProps: Object,
   nextState?: Object
-): boolean =>
-  !isEqualWith(instance.props, nextProps, customizer) ||
-  !isEqualWith(instance.state, nextState, customizer)
+): boolean => {
+  const propsEqual = isEqualWith(instance.props, nextProps, customizer)
+  const stateEqual = isEqualWith(instance.state, nextState, customizer)
+
+  return !propsEqual || !stateEqual
+}
 
 export default shallowCompare

src/__tests__/Field.spec.js
@@ -914,6 +914,52 @@ const describeField = (name, structure, combineReducers, setup) => {
       expect(input).toHaveBeenCalledTimes(1)
     })
 
+    it('The render() function should not be called if neither state nor props of the field has changed', () => {
+      const store = makeStore()
+      const input = jest.fn(props => <input {...props.input} />)
+      const renderSpy = jest.fn()
+
+      class TestField extends Field {
+        render() {
+          renderSpy()
+          return super.render()
+        }
+      }
+
+      class Form extends Component {
+        constructor() {
+          super()
+          this.state = { foo: 'bar' }
+        }
+
+        render() {
+          return (
+            <div>
+              <TestField name="myField" component={input} />
+              <button onClick={() => this.setState({ foo: 'qux' })}>
+                Change
+              </button>
+            </div>
+          )
+        }
+      }
+      const TestForm = reduxForm({ form: 'testForm' })(Form)
+      const dom = TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <TestForm />
+        </Provider>
+      )
+
+      expect(renderSpy).toHaveBeenCalledTimes(1)
+      expect(input).toHaveBeenCalledTimes(1)
+
+      const button = TestUtils.findRenderedDOMComponentWithTag(dom, 'button')
+      TestUtils.Simulate.click(button)
+
+      expect(renderSpy).toHaveBeenCalledTimes(1)
+      expect(input).toHaveBeenCalledTimes(1)
+    })
+
     it('should call normalize function on change', () => {
       const store = makeStore({
         testForm: {